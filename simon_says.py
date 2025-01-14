99fc1a3f635d6902168322cb76cf730d4f1cc8c1
Achira03 <achizx0@gmail.com>
2025-01-14 17:18:28 +0700

    import module for this game

---
+++ simon_says.py
@@ -0,0 +1,8 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.clock import Clock
+import random
+
